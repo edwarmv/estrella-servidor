219849ffa3fb235b9b0559a2d2cb4b29a21232f1
Edwar Martinez <edwar_kenedy@hotmail.com>
2020-11-22 21:42:35 -0400

    Corregido problema de nombres repetidos en producto.seed

--- src/seeds/producto.seed.ts
+++ src/seeds/producto.seed.ts
@@ -1,9 +1,19 @@
 import { Seeder, Factory } from 'typeorm-seeding';
 import { Connection } from 'typeorm';
 import { Producto } from 'entities/producto';
+import faker from 'faker';
 
 export default class CrearProducto implements Seeder {
   public async run(factory: Factory, connection: Connection): Promise<void> {
-    await factory(Producto)().createMany(100);
+    const nombres: string[] = [];
+    for(let i = 0; i < 100; i++) {
+      nombres.push(faker.commerce.productName());
+    }
+    const nombresNoRepetidos = nombres.filter((item, index) => {
+      return nombres.indexOf(item) === index;
+    });
+    nombresNoRepetidos.forEach(async nombre => {
+      await factory(Producto)().create({ nombre });
+    });
   }
 }
